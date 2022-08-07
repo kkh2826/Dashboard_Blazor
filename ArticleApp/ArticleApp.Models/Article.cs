d79f91bc5d902f4775d4fc1a694c050d64b2fca9
kyounghoon <kkh2826@naver.com>
2022-08-07 19:53:56 +0900

    Make Article Local DB And Add Article Class File

---
+++ ArticleApp/ArticleApp.Models/Article.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ArticleApp.Models
+{
+    public class Article
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Title { get; set; }
+    }
+}
---
+++ ArticleApp/ArticleApp.SqlServer/dbo/Tables/Articles.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Articles]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY(1,1), -- 일련번호
+	[Title] NVARCHAR(255) NOT NULL,				 -- 제목
+)
+
+GO
