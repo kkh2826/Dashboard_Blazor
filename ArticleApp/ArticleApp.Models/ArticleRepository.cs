c7b0c09a79c66bc239b3598c5e606b1df92783d3
kyounghoon <kkh2826@naver.com>
2022-08-07 20:02:43 +0900

    Add Article Repository And Interface And Install Dul For Paging

---
+++ ArticleApp/ArticleApp.Models/ArticleRepository.cs
@@ -0,0 +1,37 @@
+﻿using Dul.Domain.Common;
+
+namespace ArticleApp.Models
+{
+    public class ArticleRepository : IArticleRepository
+    {
+        public Task<Article> AddArticleAsync(Article article)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteArticleAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Article> EditArticleAsync(Article article)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<PagingResult<Article>> GetAllAsync(int pageIndex, int pageSize)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Article> GetArticleByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Article>> GetArticlesAsync()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ ArticleApp/ArticleApp.Models/IArticleRepository.cs
@@ -0,0 +1,15 @@
+﻿using Dul.Domain.Common;
+
+namespace ArticleApp.Models
+{
+    public interface IArticleRepository
+    {
+        Task<Article> AddArticleAsync(Article article); // 입력
+        Task<List<Article>> GetArticlesAsync();         // 출력
+        Task<Article> GetArticleByIdAsync(int id);      // 특정 개체
+        Task<Article> EditArticleAsync(Article article); // 수정
+        Task DeleteArticleAsync(int id);                // 삭제
+
+        Task<PagingResult<Article>> GetAllAsync(int pageIndex, int pageSize); // 페이징 관련 메서드
+    }
+}
