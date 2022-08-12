d309c2752a91cafac4e8470b0fbaf087a0c9ecb1
kyounghoon <kkh2826@naver.com>
2022-08-12 21:20:40 +0900

    Apply Code Behind Type With Index/Create

---
+++ ArticleApp/ArticleApp/Pages/Articles/Create.razor.cs
@@ -0,0 +1,23 @@
+﻿using ArticleApp.Models.Articles;
+using Microsoft.AspNetCore.Components;
+
+namespace ArticleApp.Pages.Articles
+{
+    public partial class Create
+    {
+        [Inject]
+        public NavigationManager NavigationManager { get; set; }
+        [Inject]
+        public IArticleRepository ArticleRepository { get;set; }
+        public Article Model { get; set; } = new Article();
+
+        protected async Task btnSubmit_Click()
+        {
+            // 저장 로직
+            await ArticleRepository.AddArticleAsync(Model);
+
+            // 리스트 페이지 이동
+            NavigationManager.NavigateTo("/Articles");
+        }
+    }
+}
---
+++ ArticleApp/ArticleApp/Pages/Articles/Index.razor.cs
@@ -0,0 +1,47 @@
+﻿using ArticleApp.Models.Articles;
+using Dul.Domain.Common;
+using Dul.Web;
+using Microsoft.AspNetCore.Components;
+
+namespace ArticleApp.Pages.Articles
+{
+    public partial class Index
+    {
+        [Inject]
+        public IArticleRepository ArticleRepository { get; set; }
+
+        // 페이저 기본값 설정
+        private PagerBase pager = new PagerBase()
+        {
+            PageNumber = 1,
+            PageIndex = 0,
+            PageSize = 2,
+            PagerButtonCount = 5
+        };
+
+        private List<Article> articles;
+
+        protected override async Task OnInitializedAsync()
+        {
+            // 전체 데이터 모두 출력
+            //articles = await ArticleRepository.GetArticlesAsync();
+
+            // 페이징 처리된 데이터만 출력
+            PagingResult<Article> pagingData = await ArticleRepository.GetAllAsync(pager.PageIndex, pager.PageSize);
+            pager.RecordCount = pagingData.TotalRecords; // 총 레코드 수
+            articles = pagingData.Records.ToList(); // 페이징 처리된 레코드
+        }
+
+        private async void PageIndexChanged(int pageIndex)
+        {
+            pager.PageIndex = pageIndex;
+            pager.PageNumber = pageIndex + 1;
+
+            var pagingData = await ArticleRepository.GetAllAsync(pager.PageIndex, pager.PageSize);
+            pager.RecordCount = pagingData.TotalRecords; // 총 레코드 수
+            articles = pagingData.Records.ToList(); // 페이징 처리된 레코드
+
+            StateHasChanged();
+        }
+    }
+}
