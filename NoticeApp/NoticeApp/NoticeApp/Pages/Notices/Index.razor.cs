3720caf3c34cbfde14e9b12a4639cc023937d5d9
kyounghoon <kkh2826@naver.com>
2022-08-14 12:43:16 +0900

    Add NoticeApp And Create Page With CodeBehind And Apply MatBlazr

---
+++ NoticeApp/NoticeApp/NoticeApp/Pages/Notices/Index.razor.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Components;
+using NoticeApp.Models;
+
+namespace NoticeApp.Pages.Notices
+{
+    public partial class Index
+    {
+        [Inject]
+        public INoticeRepositoryAsync NoticeRepositoryAsyncReference { get; set; }
+
+        [Inject]
+        public NavigationManager NavigationManagerReference { get; set; }
+
+        protected List<Notice> models;
+
+        protected DulPager.DulPagerBase pager = new DulPager.DulPagerBase()
+        {
+            PageNumber = 1,
+            PageIndex = 0,
+            PageSize = 2,
+            PagerButtonCount = 5
+            
+        };
+
+        protected override async Task OnInitializedAsync()
+        {
+            await DisplayData();
+        }
+
+        private async Task DisplayData()
+        {
+            //await Task.Delay(3000);
+            var resultSet = await NoticeRepositoryAsyncReference.GetAllAsync(pager.PageIndex, pager.PageSize);
+            pager.RecordCount = resultSet.TotalRecords;
+            models = resultSet.Records.ToList();
+
+        }
+
+        protected void NameClick(int id)
+        {
+            NavigationManagerReference.NavigateTo($"/Notices/Details/{id}");
+        }
+
+        protected async void PageIndexChanged(int pageIndex)
+        {
+            pager.PageIndex = pageIndex;
+            pager.PageNumber = pageIndex + 1;
+
+            await DisplayData();
+
+            StateHasChanged();
+        }
+
+    }
+}
