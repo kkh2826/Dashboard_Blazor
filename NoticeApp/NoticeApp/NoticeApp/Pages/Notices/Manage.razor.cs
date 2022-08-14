8b3a571bbc7be97986cd7724c978db0519023925
kyounghoon <kkh2826@naver.com>
2022-08-14 18:07:59 +0900

    Apply Manage Page And EditorForm

--- NoticeApp/NoticeApp/NoticeApp/Pages/Notices/Manage.razor.cs
+++ NoticeApp/NoticeApp/NoticeApp/Pages/Notices/Manage.razor.cs
@@ -1,10 +1,85 @@
-﻿namespace NoticeApp.Pages.Notices
+﻿using Microsoft.AspNetCore.Components;
+using NoticeApp.Models;
+using NoticeApp.Pages.Notices.Components;
+
+namespace NoticeApp.Pages.Notices
 {
     public partial class Manage
     {
-        protected override void OnInitialized()
+        [Inject]
+        public INoticeRepositoryAsync NoticeRepositoryAsyncReference { get; set; }
+
+        [Inject]
+        public NavigationManager NavigationManagerReference { get; set; }
+
+        public EditorForm EditorFormReference { get; set; }
+
+        protected List<Notice> models;
+
+        protected Notice model = new Notice();
+
+        protected DulPager.DulPagerBase pager = new DulPager.DulPagerBase()
         {
+            PageNumber = 1,
+            PageIndex = 0,
+            PageSize = 2,
+            PagerButtonCount = 5
+
+        };
+
+        protected override async Task OnInitializedAsync()
+        {
+            await DisplayData();
+        }
+
+        private async Task DisplayData()
+        {
+            //await Task.Delay(3000);
+            var resultSet = await NoticeRepositoryAsyncReference.GetAllAsync(pager.PageIndex, pager.PageSize);
+            pager.RecordCount = resultSet.TotalRecords;
+            models = resultSet.Records.ToList();
 
         }
+
+        protected void NameClick(int id)
+        {
+            NavigationManagerReference.NavigateTo($"/Notices/Details/{id}");
+        }
+
+        protected async void PageIndexChanged(int pageIndex)
+        {
+            pager.PageIndex = pageIndex;
+            pager.PageNumber = pageIndex + 1;
+
+            await DisplayData();
+
+            StateHasChanged();
+        }
+
+        public string EditorFormTitle { get; set; } = "CREATE";
+
+        protected void ShowEditorForm()
+        {
+            EditorFormTitle = "CREATE";
+            this.model = new Notice();
+            EditorFormReference.Show();
+        }
+
+        protected void EditBy(Notice model)
+        {
+            EditorFormTitle = "EDIT";
+            this.model = new Notice();
+            this.model = model;
+            EditorFormReference.Show();
+        }
+
+        protected async void CreateOrEdit()
+        {
+            EditorFormReference.Hide();
+            this.model = new Notice();
+            await DisplayData();
+            StateHasChanged();
+        }
+
     }
 }
